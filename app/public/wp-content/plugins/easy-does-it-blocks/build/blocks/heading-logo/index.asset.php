807159f7ce1d5cf323b87ed302a7b0c2bef00e41
PAMulligan <paul.mulliganiel@hey.com>
2023-09-15 09:00:59 -0400

    Work on functionality

--- app/public/wp-content/plugins/easy-does-it-blocks/build/blocks/heading-logo/index.asset.php
+++ app/public/wp-content/plugins/easy-does-it-blocks/build/blocks/heading-logo/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '34b036dbc9d80f04f532');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '825ebd4d4bb9ba3a0644');
